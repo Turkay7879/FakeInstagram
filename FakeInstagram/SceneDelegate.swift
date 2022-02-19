f2dab35aed9b0a2d7ce32df4ed6c6dc7f73218dc
Türkay <turkay@Turkays-MacBook-Air.local>
2022-02-20 01:15:21 +0300

    Check if an existing user already logged in

--- FakeInstagram/SceneDelegate.swift
+++ FakeInstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,6 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        // There's a user that's already signed in. No need to ask for their credentials again
+        if Auth.auth().currentUser != nil {
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            let tabBarVC = storyBoard.instantiateViewController(identifier: "tabBarController")
+            window?.rootViewController = tabBarVC
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
